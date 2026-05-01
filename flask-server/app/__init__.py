45db8eed204edf3aef0155003d0c570a5dd530f9
slowlyP <vvckfn@gmail.com>
2026-05-01 16:06:23 +0900

    feat: add realtime notification socket scaffold (#30)

--- flask-server/app/__init__.py
+++ flask-server/app/__init__.py
@@ -1,4 +1,4 @@
-﻿from flask import Flask
+from flask import Flask
 
 from app.config import Config
 from app.extensions import init_extensions
@@ -10,6 +10,7 @@ from app.routes.incident_routes import incident_bp
 from app.routes.notification_routes import notification_bp
 from app.routes.report_routes import report_bp
 from app.routes.llm_report_routes import llm_report_bp
+from app.sockets import register_socket_events
 
 
 def create_app():
@@ -17,6 +18,7 @@ def create_app():
     app.config.from_object(Config)
 
     init_extensions(app)
+    register_socket_events()
 
     app.register_blueprint(health_bp)
     app.register_blueprint(auth_bp)
---
+++ flask-server/app/sockets/__init__.py
@@ -0,0 +1,2 @@
+def register_socket_events():
+    from app.sockets import notification_socket  # noqa: F401
